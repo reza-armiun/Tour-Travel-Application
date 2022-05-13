d9b299c899bc9bc88dc3f20a92a2453032741217
Raze-Arm <79927358+Raze-Arm@users.noreply.github.com>
2022-05-13 18:51:43 +0430

    fixes command line table generator

--- controller/commandline/TableGenerator.java
+++ controller/commandline/TableGenerator.java
@@ -1,10 +1,8 @@
 package razarm.tosan.controller.commandline;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 public class TableGenerator {
 
